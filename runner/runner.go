a7db3459a0b762c709757e2324a41d237c4450e6
Diego Garcia <drgarcia1986@gmail.com>
2018-04-18 18:26:12 -0300

    Add runner pkg to run rey forever

---
+++ runner/runner.go
@@ -0,0 +1,60 @@
+package runner
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/luizalabs/rey/aggregator"
+	"github.com/luizalabs/rey/checker"
+	"github.com/luizalabs/rey/component"
+	"golang.org/x/sync/errgroup"
+)
+
+type Runner struct {
+	cc     *checker.Checker
+	ag     *aggregator.Aggregator
+	ticker *time.Ticker
+}
+
+func (r *Runner) Run(ctx context.Context, compList []*component.Component) error {
+	if len(compList) == 0 {
+		return fmt.Errorf("empty component list\n")
+	}
+
+	for range r.ticker.C {
+		g, _ := errgroup.WithContext(ctx)
+		for _, c := range compList {
+			comp := c
+			g.Go(func() error {
+				st, err := r.cc.Check(comp)
+				if err != nil {
+					return err
+				}
+				if st.StatusID == comp.LastStatus {
+					return nil
+				}
+				if err := r.ag.Report(st); err != nil {
+					return err
+				}
+
+				comp.LastStatus = st.StatusID
+				comp.LastDetail = st.Details
+				return nil
+			})
+		}
+		if err := g.Wait(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *Runner) Stop() {
+	r.ticker.Stop()
+}
+
+func New(interval int, cc *checker.Checker, ag *aggregator.Aggregator) *Runner {
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+	return &Runner{cc: cc, ag: ag, ticker: ticker}
+}
