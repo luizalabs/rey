2b64d9ac0918e2a7e117386a576584ed404e5d93
Diego Garcia <drgarcia1986@gmail.com>
2018-04-23 11:30:28 -0300

    Add `guage` pkg with prometheus Gauge and prometheus http server

---
+++ gauge/gauge.go
@@ -0,0 +1,42 @@
+package gauge
+
+import (
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	mu              = new(sync.Mutex)
+	registredGauges = map[string]prometheus.Gauge{}
+)
+
+type Gauge struct {
+	pg prometheus.Gauge
+}
+
+func (g *Gauge) Set(n float64) {
+	g.pg.Set(n)
+}
+
+func New(compName string) *Gauge {
+	return &Gauge{pg: getOrCreatePG(compName)}
+}
+
+func getOrCreatePG(compName string) prometheus.Gauge {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if g, found := registredGauges[compName]; found {
+		return g
+	}
+	g := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:        "rey",
+		Help:        "Doctor Rey the Health Checker",
+		ConstLabels: prometheus.Labels{"component_name": compName},
+	})
+	prometheus.Register(g)
+	registredGauges[compName] = g
+
+	return g
+}
---
+++ gauge/server.go
@@ -0,0 +1,33 @@
+package gauge
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewServer(port string) *Server {
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+
+	return &Server{
+		httpServer: &http.Server{
+			Addr:    fmt.Sprintf(":%s", port),
+			Handler: mux,
+		},
+	}
+}
+
+func (s *Server) Run() error {
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) {
+	s.httpServer.Shutdown(ctx)
+}
